e3457740182f6425bbdcc2f646567e7d9d0ebd9e
Kavindu Dasanayaka <Kavindudasanayaka23@gmail.com>
2026-04-27 00:29:35 +0530

    first commit

---
+++ saucedemo-qa-automation-playwright/playwright.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: false,
+    screenshot: 'only-on-failure',
+    screenshotPath: 'screenshots',
+    video: 'off',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+
+});
+
